fdc3d16344ff3eec4012c9e76a4b76343e58aad5
Anna Maria Heliasz <annemaryheliasz@gmail.com>
2025-01-11 14:27:01 +0100

    establish db connection

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 import express from 'express';
+import { dbConnection } from './config/db';
+
 const app = express();
 const PORT = 3000;
+
+dbConnection();
 
 app.use(express.urlencoded( {extended: true}));
 app.use(express.static("public"));
