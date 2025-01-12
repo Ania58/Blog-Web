6f2bbf4167c710b791814a2376e60c59664d4ed1
Anna Maria Heliasz <annemaryheliasz@gmail.com>
2025-01-12 15:14:10 +0100

    create more functions for authentication in authController and correct the existing ones

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,3 +1,4 @@
+import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
 import admin from "firebase-admin";
 const auth = admin.auth();
 
@@ -8,20 +9,26 @@ const registerUser = async (req, res) => {
       res.redirect('/login');
     } catch (error) {
       console.error('Error creating new user:', error);
-      res.redirect('/register');
+      res.render("register.ejs", { error: "User could not be created. Try again!" });
     }
   };
   
   const loginUser = async (req, res) => {
-    const { idToken } = req.body;
-    try {
-      await auth.verifyIdToken(idToken);
-      res.cookie('token', idToken, { httpOnly: true, secure: false });
-      res.json({ success: true });
-    } catch (error) {
-      console.error('Error verifying ID token:', error);
-      res.status(401).json({ error: 'Invalid token' });
-    }
+    const { email, password } = req.body;
+
+  try {
+    const clientAuth = getAuth(); 
+    const userCredential = await signInWithEmailAndPassword(clientAuth, email, password);
+    const idToken = await userCredential.user.getIdToken(); 
+
+    await auth.verifyIdToken(idToken);
+
+    res.cookie('token', idToken, { httpOnly: true, secure: false });
+    res.redirect('/dashboard');
+  } catch (error) {
+    console.error('Error during login:', error);
+    res.render("login.ejs", { error: "Invalid credentials. Please try again!" });
+  }
   };
 
 
@@ -30,5 +37,14 @@ const registerUser = async (req, res) => {
     res.redirect('/login');
   };
 
+ 
+const registerForm = (req, res) => {
+    res.render("register.ejs", { error: null });
+  };
   
-  export { registerUser, loginUser, logoutUser }
+ 
+  const loginForm = (req, res) => {
+    res.render("login.ejs", { error: null });
+  };
+  
+  export { registerUser, loginUser, logoutUser, registerForm, loginForm }
